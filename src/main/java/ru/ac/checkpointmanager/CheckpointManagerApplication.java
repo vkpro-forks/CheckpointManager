26c791f2979d4d540cf1d406db2fe3dab045e5e8
Ldv236 <Ldv236@yandex.ru>
2023-07-21 23:07:02 +0200

    initial commit real

---
+++ src/main/java/ru/ac/checkpointmanager/CheckpointManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.ac.checkpointmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckpointManagerApplication.class, args);
+    }
+
+}
