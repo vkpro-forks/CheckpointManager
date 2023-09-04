1727d7c331936619d53f46fbe3d095648afc5e49
x3imal <aasurov@gmail.com>
2023-09-04 11:15:40 +0300

    удалил нафиг тесты все пока что, добавил отображение версии в свагер

--- src/main/java/ru/ac/checkpointmanager/configuration/OpenApiConfig.java
+++ src/main/java/ru/ac/checkpointmanager/configuration/OpenApiConfig.java
@@ -2,7 +2,6 @@ package ru.ac.checkpointmanager.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
