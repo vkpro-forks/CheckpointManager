aec03285d0d704ed49a77191f503872d5abc6497
x3imal <aasurov@gmail.com>
2023-09-25 10:24:22 +0300

    add CORS mapping and API Swagger config localhost

--- src/main/java/ru/ac/checkpointmanager/configuration/OpenApiConfig.java
+++ src/main/java/ru/ac/checkpointmanager/configuration/OpenApiConfig.java
@@ -17,6 +17,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8080"))
                 .addServersItem(new Server().url("https://checkpoint-manager.ru"))
                 .info(new Info().title("Checkpoint Manager")
                         .description("Наши бэкэндеры лучшие! Не забудем и о тестерах и фронтах конечно!")
