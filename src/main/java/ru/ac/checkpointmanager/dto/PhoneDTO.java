d1a3bc89cc6de574dee0633c721a0b0956bb1d20
fifimova <fi_fimova@mail.ru>
2023-10-09 15:49:17 +0300

    #25 -fix QA suggestions -add equals and hashcode

--- src/main/java/ru/ac/checkpointmanager/dto/PhoneDTO.java
+++ src/main/java/ru/ac/checkpointmanager/dto/PhoneDTO.java
@@ -1,6 +1,7 @@
 package ru.ac.checkpointmanager.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import ru.ac.checkpointmanager.model.enums.PhoneNumberType;
@@ -16,8 +17,10 @@ public class PhoneDTO {
     @Size(min = 11, max = 20)
     private String number;
 
+    @NotNull
     private PhoneNumberType type;
 
+    @NotNull
     private UUID userId;
 
     private String note;
