2300a3a9c23c883227f37aba847a7a3a1afb1f08
x3imal <aasurov@gmail.com>
2023-10-09 11:49:12 +0300

    51: fix mapper toCrossing, fix CrossingDTO, fixCrossingServiceImpl(optimized code, del some new Object)

--- src/main/java/ru/ac/checkpointmanager/service/CrossingServiceImpl.java
+++ src/main/java/ru/ac/checkpointmanager/service/CrossingServiceImpl.java
@@ -30,73 +30,76 @@ public class CrossingServiceImpl implements CrossingService {
     private final CheckpointRepository checkpointRepository;
     private final Logger logger = LoggerFactory.getLogger(CrossingServiceImpl.class);
 
-    @Override //логика сложная, распишу сразу тут, чтоб было понятно
+    @Override
     public Crossing markCrossing(Crossing crossing) {
+        Pass pass = validatePass(crossing.getPass().getId());
+        Checkpoint checkpoint = validateCheckpoint(crossing.getCheckpoint().getId(), pass.getTerritory().getId());
 
-
-        //проверяем в бд есть ли такой пропуск или нет, если нет, выбрасываем исключение
-        Pass pass = passRepository.findById(crossing.getPass().getId()).orElseThrow(
-                () -> new PassNotFoundException("There is no such pass!"));
-
-        //проверяем активен ли пропуск
-        if (pass.getStatus() != PassStatus.ACTIVE) {
-            throw new InactivePassException("The pass is not active");
-        }
-
-        Checkpoint checkpoint = checkpointRepository.findById(crossing.getCheckpoint().getId())
-                .orElseThrow(() -> new CheckpointNotFoundException("Checkpoint not found"));
-
-        UUID territoryIdFromPass = pass.getTerritory().getId();
-        UUID territoryIdFromCheckpoint = checkpoint.getTerritory().getId();
-        if (!territoryIdFromPass.equals(territoryIdFromCheckpoint)) {
-            throw new MismatchedTerritoryException("The checkpoint does not belong to the territory of the pass");
-        }
-
-        //Используем Optional что дать возможность оставить пустую переменную, чтоб избежать исключения NullPointerException
-        //проверяем было ли уже пересечение
         Optional<Crossing> lastCrossingOpt = crossingRepository.findTopByPassOrderByIdDesc(pass);
-
         validateCrossing(crossing.getDirection(), lastCrossingOpt, crossing.getPass().getId());
 
-        if (pass.getTypeTime() == PassTypeTime.ONETIME) {
-            if (lastCrossingOpt.isPresent()) {
-                Crossing lastCrossing = lastCrossingOpt.get();
+        manageOneTimePass(crossing.getDirection(), lastCrossingOpt, pass);
 
-                //вот это сложное условия :))) проверяет, являлось ли последнее пересечение-въездом, если пытаются въехать еще раз, выдаем ошибку
-                if (lastCrossing.getDirection().equals(Direction.IN) && crossing.getDirection().equals(Direction.IN)) {
-                    throw new EntranceWasAlreadyException(String.format("The %s has already been used for entry.", crossing.getPass().getId()));
-                }
-            } else if (crossing.getDirection() == Direction.OUT) {
-                throw new InactivePassException(String.format("This %s has not been activated (login to activate)", crossing.getPass().getId()));
-            }
-
-            if (crossing.getDirection().equals(Direction.OUT)) {
-                passRepository.completedStatusById(pass.getId());
-            }
-        }
-
-
-        crossing = new Crossing();
         crossing.setPass(pass);
         crossing.setCheckpoint(checkpoint);
-        crossing.setLocalDateTime(crossing.getLocalDateTime());
-        crossing.setDirection(crossing.getDirection());
 
         return crossingRepository.save(crossing);
     }
 
+    // проверяет, существует ли пропуск с данным ID и активен ли он
+    private Pass validatePass(UUID passId) {
+        return passRepository.findById(passId)
+                .filter(p -> p.getStatus() == PassStatus.ACTIVE)
+                .orElseThrow(() -> new InactivePassException("The pass is not active"));
+    }
 
-    // Метод для валидации пересечений
+    //проверяет, существует ли КПП с данным ID и принадлежит ли он территории пропуска
+    private Checkpoint validateCheckpoint(UUID checkpointId, UUID territoryIdFromPass) {
+        return checkpointRepository.findById(checkpointId)
+                .filter(cp -> cp.getTerritory().getId().equals(territoryIdFromPass))
+                .orElseThrow(() -> new MismatchedTerritoryException("The checkpoint does not belong to the territory of the pass"));
+    }
+
+    //логико для одноразовых пропусков(не был ли уже использован пропуск для въезда, активирован ли пропуск(для случая выезда без предварительного въезда))
+    //если направление — выезд, меняет статус пропуска на "завершенный"
+    private void manageOneTimePass(Direction currentDirection, Optional<Crossing> lastCrossingOpt, Pass pass) {
+        if (pass.getTypeTime() == PassTypeTime.ONETIME) {
+            if (isDoubleEntry(currentDirection, lastCrossingOpt)) {
+                throw new EntranceWasAlreadyException(String.format("The %s has already been used for entry.", pass.getId()));
+            }
+
+            if (isInvalidOutEntry(currentDirection, lastCrossingOpt)) {
+                throw new InactivePassException(String.format("This %s has not been activated (login to activate)", pass.getId()));
+            }
+
+            if (currentDirection.equals(Direction.OUT)) {
+                passRepository.completedStatusById(pass.getId());
+            }
+        }
+    }
+
+    //проверяет последнее пересечение для данного пропуска, если оно существует
+    //т.е. проверяет, не пытается ли пользователь проехать/пройти два раза в одном и том же направлении
     private void validateCrossing(Direction direction, Optional<Crossing> lastCrossingOpt, UUID passId) {
-        // Проверяем, есть ли последнее пересечение
         if (lastCrossingOpt.isPresent()) {
             Crossing lastCrossing = lastCrossingOpt.get();
 
-            // Проверяем, чтобы въезд и выезд чередовались
             if (lastCrossing.getDirection().equals(direction)) {
                 throw new IllegalStateException(String.format("This passId %s needs to check in/check out", passId));
             }
         }
+    }
+
+    //проверяет, не пытается ли пользователь въехать дважды на территорию с одноразовым пропуском
+    private boolean isDoubleEntry(Direction currentDirection, Optional<Crossing> lastCrossingOpt) {
+        return lastCrossingOpt
+                .filter(lastCrossing -> currentDirection.equals(Direction.IN) && lastCrossing.getDirection().equals(Direction.IN))
+                .isPresent();
+    }
+
+    //проверяет, не пытается ли пользователь выехать без активации пропуска (т.е. без въезда)
+    private boolean isInvalidOutEntry(Direction currentDirection, Optional<Crossing> lastCrossingOpt) {
+        return currentDirection == Direction.OUT && lastCrossingOpt.isEmpty();
     }
 }
 
