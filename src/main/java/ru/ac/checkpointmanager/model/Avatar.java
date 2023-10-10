1a1e57255b9f51a9cf29158ae483549357e902a9
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-10-10 13:23:13 +0300

    66/feat: create avatar entity class

---
+++ src/main/java/ru/ac/checkpointmanager/model/Avatar.java
@@ -0,0 +1,38 @@
+package ru.ac.checkpointmanager.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Avatar entity class. There is no fixed list of entities
+ * that can have avatar.
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "avatars")
+public class Avatar {
+
+    /**
+     * UUID of Entity related to the avatar
+     */
+    @Id
+    @Column(name = "avatar_holder")
+    private UUID avatarHolder;
+
+    @Column(name = "media_type")
+    private String mediaType;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "file_size")
+    private long fileSize;
+
+    @Column(name = "preview")
+    private byte[] preview;
+}
