54e13e85beebe59b77d2117f5a42a691b8368fad
x3imal <aasurov@gmail.com>
2023-11-07 18:38:23 +0300

    91: фикс отображение ошибки при невалидном номере

--- src/main/java/ru/ac/checkpointmanager/dto/CarDTO.java
+++ src/main/java/ru/ac/checkpointmanager/dto/CarDTO.java
@@ -1,9 +1,12 @@
 package ru.ac.checkpointmanager.dto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import ru.ac.checkpointmanager.model.car.CarBrand;
 
@@ -16,6 +19,9 @@ public class CarDTO {
     private UUID id;
 
     @NotNull
+    @Column(name = "license_plate")
+    @Size(min = 6, max = 10, message = "The number must be at least 6 characters and no more than 10")
+    @Pattern(regexp = "^[АВЕКМНОРСТУХA-Z0-9]+$", message = "Invalid characters in license plate")
     private String licensePlate;
 
     private CarBrand brand;
