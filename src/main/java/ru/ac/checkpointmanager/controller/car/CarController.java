5b7cb43ad780fe6d3390c3f11931e685577affa6
Veselov Nikolay <veselovnd@gmail.com>
2023-11-24 14:24:52 +0300

    feature/131: Validated annotation for validation:)

--- src/main/java/ru/ac/checkpointmanager/controller/car/CarController.java
+++ src/main/java/ru/ac/checkpointmanager/controller/car/CarController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("chpman/car")
 @RequiredArgsConstructor
+@Validated
 @SecurityRequirement(name = "bearerAuth")
 @Tag(name = "Car (Машина)", description = "Для обработки списка машин")
 @ApiResponses(value = {@ApiResponse(responseCode = "401", description = "UNAUTHORIZED: пользователь не авторизован"),
