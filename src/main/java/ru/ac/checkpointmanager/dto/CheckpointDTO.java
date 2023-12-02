c2e0f59545b99b26f8426a56dcbcc0b84efdae70
Nikolay <veselovnd@gmail.com>
2023-12-02 22:50:08 +0300

    feature/151: + fixes after merge master

--- src/main/java/ru/ac/checkpointmanager/dto/CheckpointDTO.java
+++ src/main/java/ru/ac/checkpointmanager/dto/CheckpointDTO.java
@@ -3,12 +3,14 @@ package ru.ac.checkpointmanager.dto;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.ac.checkpointmanager.model.checkpoints.CheckpointType;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CheckpointDTO {
 
     private UUID id;
