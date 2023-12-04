d07a9f888fefe35ed40a6a1f039dc936d948c13d
x3imal <aasurov@gmail.com>
2023-12-04 13:30:15 +0300

    161: добавил настройку СCORS для фронтов(для теста).

--- src/main/java/ru/ac/checkpointmanager/configuration/SecurityConfig.java
+++ src/main/java/ru/ac/checkpointmanager/configuration/SecurityConfig.java
@@ -101,6 +101,7 @@ public class SecurityConfig {
                 "http://192.168.1.37:3000", //Сонин Ip (удалить потом)
                 "http://192.168.31.74:3000", //Сони Ip (удалить потом)
                 "http://192.168.1.55:3000", //Жени Ip(удалить потом)
+                "https://checkpoint-manager.vercel.app", //Приложение для фронта(для тестирования).
                 "https://checkpoint-manager.ru",
                 "https://www.checkpoint-manager.ru"
         ));
