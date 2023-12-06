ac018688c45e56eb952e765516db14f39a80913d
x3imal <aasurov@gmail.com>
2023-12-06 19:14:56 +0300

    174: изменил скрипт тот был неправильный :)

--- src/main/resources/liquibase/scripts/V_174__change_column_brand_id.sql
+++ src/main/resources/liquibase/scripts/V_174__change_column_brand_id.sql
@@ -1,7 +1,6 @@
 -- liquibase formatted sql
 
 -- changeset x3imal:174
-ALTER TABLE car_brand ALTER COLUMN id TYPE BIGINT;
 
 ALTER TABLE cars ALTER COLUMN brand_id  TYPE BIGINT;
 
@@ -9,5 +8,4 @@ ALTER TABLE cars ALTER COLUMN brand_id DROP DEFAULT;
 
 DROP SEQUENCE IF EXISTS cars_brand_id_seq;
 
-ALTER TABLE cars ALTER COLUMN brand_id SET DEFAULT nextval('car_brand_id_seq');
 
