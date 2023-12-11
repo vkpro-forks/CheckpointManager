a94ec8a0905b1964b7f818646a725896e3e5e767
fifimova <fi_fimova@mail.ru>
2023-12-11 20:49:48 +0300

    - рефакторинг тестов для UserServiceImpl - попробовала хваленные Assertions

--- src/main/java/ru/ac/checkpointmanager/dto/ChangeEmailRequest.java
+++ src/main/java/ru/ac/checkpointmanager/dto/ChangeEmailRequest.java
@@ -1,9 +1,13 @@
 package ru.ac.checkpointmanager.dto;
 
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChangeEmailRequest {
 
     @Email
