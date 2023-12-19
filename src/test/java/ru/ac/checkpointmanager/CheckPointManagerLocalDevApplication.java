9a789df3f15435f5c018ab8ba6be8f2b5446e729
Veselov Nikolay <veselovnd@gmail.com>
2023-12-19 12:18:14 +0300

    feature/190: comment

--- src/test/java/ru/ac/checkpointmanager/CheckPointManagerLocalDevApplication.java
+++ src/test/java/ru/ac/checkpointmanager/CheckPointManagerLocalDevApplication.java
@@ -8,6 +8,9 @@ public class CheckPointManagerLocalDevApplication {
 
     /**
      * Запускает приложение с временным контейнером Postgres с базовыми настройками
+     * Важно запускать c environment: spring.profiles.active=test, чтобы взять все настройки из тестового конфига
+     * (В обычном конфиге в самом начале логбэк пытается достучаться до бд, которая указана в пропертях,
+     * а не до той, которая поднимается в контейнере
      */
     public static void main(String[] args) {
         SpringApplication.from(CheckpointManagerApplication::main)
