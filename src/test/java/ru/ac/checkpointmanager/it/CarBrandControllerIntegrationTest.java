efaeaab2614525cb007e04756c79415c8b4c1399
Nikolay <veselovnd@gmail.com>
2023-12-23 13:10:59 +0300

    bugfix/CHPBUG-9: if we want to update carbrand for the name that already exists we will receive CONFLICT ERROR

--- src/test/java/ru/ac/checkpointmanager/it/CarBrandControllerIntegrationTest.java
+++ src/test/java/ru/ac/checkpointmanager/it/CarBrandControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package ru.ac.checkpointmanager.it;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
@@ -22,6 +24,7 @@ import ru.ac.checkpointmanager.config.PostgresTestContainersConfiguration;
 import ru.ac.checkpointmanager.exception.handler.ErrorCode;
 import ru.ac.checkpointmanager.model.car.CarBrand;
 import ru.ac.checkpointmanager.repository.car.CarBrandRepository;
+import ru.ac.checkpointmanager.util.TestMessage;
 import ru.ac.checkpointmanager.util.TestUtils;
 import ru.ac.checkpointmanager.util.UrlConstants;
 
@@ -33,6 +36,7 @@ import java.util.List;
 @Import({OpenAllEndpointsTestConfiguration.class, CorsTestConfiguration.class})
 @ActiveProfiles("test")
 @WithMockUser(roles = {"ADMIN"})
+@Slf4j
 class CarBrandControllerIntegrationTest extends PostgresTestContainersConfiguration {
 
     @Autowired
@@ -50,6 +54,7 @@ class CarBrandControllerIntegrationTest extends PostgresTestContainersConfigurat
     @SneakyThrows
     void shouldSaveCarBrand() {
         String carBrandString = TestUtils.jsonStringFromObject(TestUtils.getCarBrand());
+        log.info(TestMessage.PERFORM_HTTP, HttpMethod.POST.name(), UrlConstants.CAR_BRANDS_URL);
         mockMvc.perform(MockMvcRequestBuilders.post(UrlConstants.CAR_BRANDS_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(carBrandString))
@@ -64,10 +69,11 @@ class CarBrandControllerIntegrationTest extends PostgresTestContainersConfigurat
 
     @Test
     @SneakyThrows
-    void shouldReturnConflictErrorIfCarBrandAlreadyExists() {
+    void shouldReturnConflictErrorIfCarBrandAlreadyExistsWhenCreateNew() {
         CarBrand carBrand = TestUtils.getCarBrand();
         CarBrand savedBrand = carBrandRepository.saveAndFlush(carBrand);
         String carBrandString = TestUtils.jsonStringFromObject(savedBrand);
+        log.info(TestMessage.PERFORM_HTTP, HttpMethod.POST.name(), UrlConstants.CAR_BRANDS_URL);
         mockMvc.perform(MockMvcRequestBuilders.post(UrlConstants.CAR_BRANDS_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(carBrandString))
@@ -80,5 +86,29 @@ class CarBrandControllerIntegrationTest extends PostgresTestContainersConfigurat
                         .value(Matchers.startsWith("CarBrand")));
     }
 
+    @Test
+    @SneakyThrows
+    void shouldReturnConflictErrorIfCarBrandAlreadyExistsWhenUpdateForExistingBrand() {
+        CarBrand carBrand = TestUtils.getCarBrand();
+        CarBrand savedBrand = carBrandRepository.saveAndFlush(carBrand);
+        CarBrand anotherCarBrand = new CarBrand();
+        anotherCarBrand.setBrand("BatMobile");
+        CarBrand savedAnotherCarBrand = carBrandRepository.saveAndFlush(anotherCarBrand);
+        log.info("Two brand saved in repo: {} and {}", savedBrand.getBrand(), savedAnotherCarBrand.getBrand());
+        savedAnotherCarBrand.setBrand(savedBrand.getBrand());
+        String newCarBrandString = TestUtils.jsonStringFromObject(savedAnotherCarBrand);
+        String putUrl = (UrlConstants.CAR_BRANDS_URL + "/" + savedBrand.getId());
+        log.info(TestMessage.PERFORM_HTTP, HttpMethod.PUT.name(), putUrl);
+        mockMvc.perform(MockMvcRequestBuilders.put(putUrl)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newCarBrandString))
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.jsonPath(TestUtils.JSON_ERROR_CODE)
+                        .value(ErrorCode.CONFLICT.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath(TestUtils.JSON_TITLE)
+                        .value(Matchers.startsWith("Object")))
+                .andExpect(MockMvcResultMatchers.jsonPath(TestUtils.JSON_DETAIL)
+                        .value(Matchers.startsWith("CarBrand")));
+    }
 
 }
---
+++ src/test/java/ru/ac/checkpointmanager/util/TestMessage.java
@@ -0,0 +1,10 @@
+package ru.ac.checkpointmanager.util;
+
+public class TestMessage {
+
+    public static final String PERFORM_HTTP = "Performing {} method for url: {}";
+
+    public TestMessage() {
+
+    }
+}
