7ba830dc7aad1a18381a0097f573763388c83e8f
fifimova <fi_fimova@mail.ru>
2024-01-08 17:34:18 +0300

    поменяла порядок условий в аннотации @PreAuthorize

--- src/test/java/ru/ac/checkpointmanager/it/UserControllerRolesIntegrationTest.java
+++ src/test/java/ru/ac/checkpointmanager/it/UserControllerRolesIntegrationTest.java
@@ -1,24 +1,38 @@
 package ru.ac.checkpointmanager.it;
 
 import lombok.SneakyThrows;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.ac.checkpointmanager.config.RedisAndPostgresTestContainersConfiguration;
+import ru.ac.checkpointmanager.config.security.WithMockCustomUser;
+import ru.ac.checkpointmanager.model.Territory;
 import ru.ac.checkpointmanager.model.User;
 import ru.ac.checkpointmanager.model.enums.Role;
+import ru.ac.checkpointmanager.repository.TerritoryRepository;
 import ru.ac.checkpointmanager.repository.UserRepository;
+import ru.ac.checkpointmanager.security.authfacade.AuthFacade;
 import ru.ac.checkpointmanager.util.TestUtils;
 import ru.ac.checkpointmanager.util.UrlConstants;
 
 import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -32,9 +46,17 @@ class UserControllerRolesIntegrationTest extends RedisAndPostgresTestContainersC
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    TerritoryRepository territoryRepository;
+
+    @Qualifier("userFacade")
+    @MockBean
+    AuthFacade authFacade;
+
     @AfterEach
     void clear() {
         userRepository.deleteAll();
+        territoryRepository.deleteAll();
     }
 
     @Test
@@ -50,4 +72,26 @@ class UserControllerRolesIntegrationTest extends RedisAndPostgresTestContainersC
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
+    @Test
+    @SneakyThrows
+    @WithMockCustomUser
+    void getTerritoriesByUserShouldNotCheckIdWithRoleAdmin() {
+        User user = TestUtils.getUserForDB();
+        Territory territory = TestUtils.getTerritoryForDB();
+        territoryRepository.saveAndFlush(territory);
+        List<Territory> territories = List.of(territory);
+        user.setTerritories(territories);
+        userRepository.saveAndFlush(user);
+        UUID userId = user.getId();
+
+        mockMvc.perform(MockMvcRequestBuilders.get(UrlConstants.USER_URL + "/{userId}/territories", userId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", Matchers.is(territories.size())))
+                .andExpect(jsonPath("$[*].name", Matchers.hasItem(territory.getName())))
+                .andExpect(jsonPath("$[*].id", Matchers.hasItem(territory.getId().toString())));
+
+        Mockito.verify(authFacade, Mockito.never()).isIdMatch(UUID.randomUUID());
+    }
 }
