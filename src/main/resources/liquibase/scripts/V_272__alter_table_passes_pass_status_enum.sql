16ae66f8d2cf518734a0366d163339fa6c2c821f
Veselov Nikolay <veselovnd@gmail.com>
2024-01-18 13:40:44 +0300

    feature/272: create enum type in PG

---
+++ src/main/resources/liquibase/scripts/V_272__alter_table_passes_pass_status_enum.sql
@@ -0,0 +1,27 @@
+-- т.к. теперь есть view, поменять тип столбца не просто
+DO
+$$
+-- сначала сохраняем view в переменную
+    DECLARE
+        v_pass_view_def   text;
+        DECLARE exec_text text;
+    BEGIN
+        v_pass_view_def := pg_get_viewdef('pass_in_out_view'::regclass);
+        DROP VIEW pass_in_out_view;
+
+-- удалили view, далее создается enum, меняется тип столбца на enum, переносятся предыдущие значения
+        CREATE TYPE pass_status_enum AS ENUM (
+            'DELAYED', 'ACTIVE', 'COMPLETED', 'CANCELED', 'OUTDATED', 'WARNING'
+            );
+
+        ALTER TABLE passes
+            ALTER COLUMN status TYPE pass_status_enum USING status::pass_status_enum;
+
+        exec_text := format('create view pass_in_out_view as %s',
+                            v_pass_view_def);
+        EXECUTE exec_text;
+-- далее выполняется скрипт из переменной (то что view)
+    END
+$$;
+
+GO
