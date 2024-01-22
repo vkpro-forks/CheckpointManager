6850825b3ee7308f1368697463b7c3e52bef147f
Veselov Nikolay <veselovnd@gmail.com>
2024-01-22 20:28:09 +0300

    feature/268: unit tests for visitor service #2

--- src/test/java/ru/ac/checkpointmanager/service/visitor/VisitorServiceImplTest.java
+++ src/test/java/ru/ac/checkpointmanager/service/visitor/VisitorServiceImplTest.java
@@ -21,6 +21,8 @@ import ru.ac.checkpointmanager.model.Visitor;
 import ru.ac.checkpointmanager.repository.VisitorRepository;
 import ru.ac.checkpointmanager.util.TestUtils;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -86,12 +88,131 @@ class VisitorServiceImplTest {
         Visitor visitor = TestUtils.getVisitorUnsaved();
         Mockito.when(visitorRepository.findById(TestUtils.VISITOR_ID)).thenReturn(Optional.of(visitor));
         Mockito.when(visitorRepository.save(Mockito.any())).thenReturn(visitor);
-        visitorService.updateVisitor(TestUtils.VISITOR_ID, visitorDTO);
+        VisitorDTO returnedDto = visitorService.updateVisitor(TestUtils.VISITOR_ID, visitorDTO);
 
         Mockito.verify(visitorRepository, Mockito.times(1)).save(visitorArgumentCaptor.capture());
         Visitor captured = visitorArgumentCaptor.getValue();
         checkSavedVisitorFields(captured, visitorDTO);
+        Assertions.assertThat(returnedDto).isNotNull();
     }
+
+    @Test
+    void updateVisitor_NoVisitor_ThrowException() {
+        VisitorDTO visitorDTO = TestUtils.getVisitorDTO();
+        Mockito.when(visitorRepository.findById(TestUtils.VISITOR_ID)).thenReturn(Optional.empty());
+
+        Assertions.assertThatExceptionOfType(VisitorNotFoundException.class)
+                .isThrownBy(() -> visitorService.updateVisitor(TestUtils.VISITOR_ID, visitorDTO))
+                .isInstanceOf(EntityNotFoundException.class)
+                .withMessage(ExceptionUtils.VISITOR_NOT_FOUND.formatted(TestUtils.VISITOR_ID));
+
+        Mockito.verify(visitorRepository, Mockito.never()).save(Mockito.any());
+    }
+
+    @Test
+    void deleteVisitor_AllOk_Delete() {
+        Mockito.when(visitorRepository.existsById(TestUtils.VISITOR_ID)).thenReturn(true);
+
+        visitorService.deleteVisitor(TestUtils.VISITOR_ID);
+
+        Mockito.verify(visitorRepository, Mockito.times(1)).deleteById(TestUtils.VISITOR_ID);
+    }
+
+    @Test
+    void deleteVisitor_NoVisitor_ThrowException() {
+        Mockito.when(visitorRepository.existsById(TestUtils.VISITOR_ID)).thenReturn(false);
+
+        Assertions.assertThatExceptionOfType(VisitorNotFoundException.class)
+                .isThrownBy(() -> visitorService.deleteVisitor(TestUtils.VISITOR_ID))
+                .isInstanceOf(EntityNotFoundException.class)
+                .withMessage(ExceptionUtils.VISITOR_NOT_FOUND.formatted(TestUtils.VISITOR_ID));
+
+        Mockito.verify(visitorRepository, Mockito.never()).deleteById(TestUtils.VISITOR_ID);
+    }
+
+    @Test
+    void findByNamePart_AllOk_ReturnListOfDto() {
+        Visitor visitorUnsaved = TestUtils.getVisitorUnsaved();
+        Mockito.when(visitorRepository.findByNameContainingIgnoreCase(Mockito.any()))
+                .thenReturn(List.of(visitorUnsaved));
+
+        List<VisitorDTO> found = visitorService.findByNamePart("namepart");
+
+        Assertions.assertThat(found).isEqualTo(visitorMapper.toVisitorDTOS(List.of(visitorUnsaved)));
+    }
+
+    @Test
+    void findByNamePart_NoVisitors_ReturnEmptyList() {
+        Mockito.when(visitorRepository.findByNameContainingIgnoreCase(Mockito.any()))
+                .thenReturn(Collections.emptyList());
+
+        List<VisitorDTO> found = visitorService.findByNamePart("namepart");
+
+        Assertions.assertThat(found).isEmpty();
+    }
+
+    @Test
+    void findByPhonePart_AllOk_ReturnListOfDto() {
+        Visitor visitorUnsaved = TestUtils.getVisitorUnsaved();
+        Mockito.when(visitorRepository.findByPhoneContaining(Mockito.any()))
+                .thenReturn(List.of(visitorUnsaved));
+
+        List<VisitorDTO> found = visitorService.findByPhonePart("+7916");
+
+        Assertions.assertThat(found).isEqualTo(visitorMapper.toVisitorDTOS(List.of(visitorUnsaved)));
+    }
+
+    @Test
+    void findByPhonePart_NoVisitors_ReturnEmptyList() {
+        Mockito.when(visitorRepository.findByPhoneContaining(Mockito.any()))
+                .thenReturn(Collections.emptyList());
+
+        List<VisitorDTO> found = visitorService.findByPhonePart("+7916");
+
+        Assertions.assertThat(found).isEmpty();
+    }
+
+    @Test
+    void findByPassId_AllOk_ReturnVisitorDto() {
+        Visitor visitor = TestUtils.getVisitorUnsaved();
+        Mockito.when(visitorRepository.findVisitorByPasses_Id(TestUtils.PASS_ID)).thenReturn(Optional.of(visitor));
+
+        VisitorDTO foundDto = visitorService.findByPassId(TestUtils.PASS_ID);
+
+        Assertions.assertThat(foundDto).isEqualTo(visitorMapper.toVisitorDTO(visitor));
+        Mockito.verify(visitorRepository, Mockito.times(1)).findVisitorByPasses_Id(TestUtils.PASS_ID);
+    }
+
+    @Test
+    void findByPassId_NoVisitor_ThrowException() {
+        Mockito.when(visitorRepository.findVisitorByPasses_Id(Mockito.any())).thenReturn(Optional.empty());
+
+        Assertions.assertThatExceptionOfType(VisitorNotFoundException.class)
+                .isThrownBy(() -> visitorService.findByPassId(TestUtils.PASS_ID))
+                .isInstanceOf(EntityNotFoundException.class)
+                .withMessage(ExceptionUtils.VISITOR_BY_PASS_NOT_FOUND.formatted(TestUtils.PASS_ID));
+    }
+
+    @Test
+    void findByUserId_AllOk_ReturnVisitorDto() {
+        Visitor visitor = TestUtils.getVisitorUnsaved();
+        Mockito.when(visitorRepository.findVisitorsByUserId(TestUtils.USER_ID)).thenReturn(List.of(visitor));
+
+        List<VisitorDTO> found = visitorService.findByUserId(TestUtils.USER_ID);
+
+        Assertions.assertThat(found).isEqualTo(visitorMapper.toVisitorDTOS(List.of(visitor)));
+        Mockito.verify(visitorRepository, Mockito.times(1)).findVisitorsByUserId(TestUtils.USER_ID);
+    }
+
+    @Test
+    void findByUserId_NoVisitor_ReturnEmptyList() {
+        Mockito.when(visitorRepository.findVisitorsByUserId(Mockito.any())).thenReturn(Collections.emptyList());
+
+        List<VisitorDTO> found = visitorService.findByUserId(TestUtils.USER_ID);
+
+        Assertions.assertThat(found).isEmpty();
+    }
+
 
     private void checkSavedVisitorFields(Visitor captured, VisitorDTO visitorDTO) {
         Assertions.assertThat(captured.getName()).isEqualTo(visitorDTO.getName());
