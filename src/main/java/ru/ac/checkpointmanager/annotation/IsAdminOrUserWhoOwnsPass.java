36e31437414c81cf2bfb8dd984c52584ecc0ab40
Rnd-mi <11.stepanov.01@gmail.com>
2024-02-28 15:42:46 +0300

    feat(314): PreAuthorize annotations for commonly used access types

---
+++ src/main/java/ru/ac/checkpointmanager/annotation/IsAdminOrUserWhoOwnsPass.java
@@ -0,0 +1,20 @@
+package ru.ac.checkpointmanager.annotation;
+
+import ru.ac.checkpointmanager.security.authfacade.PassAuthFacade;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to check rights of the user who tries to perform some actions with passes
+ *
+ * @see PassAuthFacade
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('ROLE_ADMIN') or @passAuthFacade.isIdMatch(#passId)")
+public @interface IsAdminOrUserWhoOwnsPass {
+}
