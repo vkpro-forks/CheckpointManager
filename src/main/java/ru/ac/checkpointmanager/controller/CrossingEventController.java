299f4e81ece4f19c6ed63a56148636aa79d875cc
Rnd-mi <11.stepanov.01@gmail.com>
2024-02-28 15:51:48 +0300

    feat(314): CrossingEventController access tuning

--- src/main/java/ru/ac/checkpointmanager/controller/CrossingEventController.java
+++ src/main/java/ru/ac/checkpointmanager/controller/CrossingEventController.java
@@ -27,8 +27,10 @@ import ru.ac.checkpointmanager.service.event.PassInOutViewService;
 
 import java.util.UUID;
 
-import static ru.ac.checkpointmanager.utils.SwaggerConstants.INTERNAL_SERVER_ERROR_MSG;
+import static ru.ac.checkpointmanager.utils.SwaggerConstants.ACCESS_ADMIN_MESSAGE;
 import static ru.ac.checkpointmanager.utils.SwaggerConstants.UNAUTHORIZED_MSG;
+import static ru.ac.checkpointmanager.utils.SwaggerConstants.INTERNAL_SERVER_ERROR_MSG;
+import static ru.ac.checkpointmanager.utils.SwaggerConstants.EVENTS_ARE_FOUND_MESSAGE;
 
 @RestController
 @RequestMapping("api/v1/events")
@@ -43,13 +45,13 @@ public class CrossingEventController {
     private final PassInOutViewService passInOutViewService;
 
     @Operation(summary = "Получить список событий по пропускам пользователя (для User)",
-            description = "Доступ: ADMIN, USER.",
+            description = "Доступ: ADMIN - события всех пользователей, USER - только свои",
             parameters = {
                     @Parameter(in = ParameterIn.QUERY, name = "page", example = "0"),
                     @Parameter(in = ParameterIn.QUERY, name = "size", example = "20")
             })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "События найдены",
+            @ApiResponse(responseCode = "200", description = EVENTS_ARE_FOUND_MESSAGE,
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                             array = @ArraySchema(schema = @Schema(implementation = PassInOutView.class)))),
             @ApiResponse(responseCode = "404", description = "Пользователь не найден")})
@@ -62,17 +64,17 @@ public class CrossingEventController {
     }
 
     @Operation(summary = "Получить список событий по конкретной территории (для Security)",
-            description = "Доступ: ADMIN, MANAGER, SECURITY.",
+            description = "Доступ: ADMIN - по всем территориям, SECURITY - только на закрепленной территории",
             parameters = {
                     @Parameter(in = ParameterIn.QUERY, name = "page", example = "0"),
                     @Parameter(in = ParameterIn.QUERY, name = "size", example = "20")
             })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "События найдены",
+            @ApiResponse(responseCode = "200", description = EVENTS_ARE_FOUND_MESSAGE,
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                             array = @ArraySchema(schema = @Schema(implementation = PassInOutView.class)))),
             @ApiResponse(responseCode = "404", description = "Территория не найдена")})
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_SECURITY')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SECURITY')")
     @GetMapping("/territories/{territoryId}")
     public Page<PassInOutView> getEventsByTerritoryId(@PathVariable UUID territoryId,
                                                       @Schema(hidden = true)
@@ -82,17 +84,17 @@ public class CrossingEventController {
     }
 
     @Operation(summary = "Получить список событий по всем привязанным к пользователю территориям (для Manager)",
-            description = "Доступ: ADMIN, MANAGER.",
+            description = "Доступ: ADMIN - по всем территориям, MANAGER, SECURITY - только на закрепленной территории",
             parameters = {
                     @Parameter(in = ParameterIn.QUERY, name = "page", example = "0"),
                     @Parameter(in = ParameterIn.QUERY, name = "size", example = "20")
             })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "События найдены",
+            @ApiResponse(responseCode = "200", description = EVENTS_ARE_FOUND_MESSAGE,
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                             array = @ArraySchema(schema = @Schema(implementation = PassInOutView.class)))),
             @ApiResponse(responseCode = "404", description = "Пользователь или территории не найдены")})
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_SECURITY')")
     @GetMapping("/users/{userId}/territories")
     public Page<PassInOutView> findEventsByUsersTerritories(@PathVariable UUID userId,
                                                             @Schema(hidden = true)
@@ -102,20 +104,19 @@ public class CrossingEventController {
     }
 
     @Operation(summary = "Получить весь список событий (для Admin)",
-            description = "Доступ: ADMIN.",
+            description = ACCESS_ADMIN_MESSAGE,
             parameters = {
                     @Parameter(in = ParameterIn.QUERY, name = "page", example = "0"),
                     @Parameter(in = ParameterIn.QUERY, name = "size", example = "20")
             })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "События найдены",
+            @ApiResponse(responseCode = "200", description = EVENTS_ARE_FOUND_MESSAGE,
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                             array = @ArraySchema(schema = @Schema(implementation = PassInOutView.class))))})
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping()
     public Page<PassInOutView> getAllEvents(@Schema(hidden = true) @Valid
-                                                 @PagingParam PagingParams pagingParams) {
+                                            @PagingParam PagingParams pagingParams) {
         return passInOutViewService.findAll(pagingParams);
     }
-
 }
