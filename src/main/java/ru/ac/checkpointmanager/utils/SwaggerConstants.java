7a6b44437c0f3d4b15cbf220588740c62a0922fe
Rnd-mi <11.stepanov.01@gmail.com>
2024-02-28 15:51:22 +0300

    const(314): swagger api constants

--- src/main/java/ru/ac/checkpointmanager/utils/SwaggerConstants.java
+++ src/main/java/ru/ac/checkpointmanager/utils/SwaggerConstants.java
@@ -50,14 +50,20 @@ public class SwaggerConstants {
     public static final String GET_ALL_CARS_MESSAGE = "Получение всех машин.";
     public static final String GET_CARS_BY_USER_MESSAGE = "Получение всех машин по user.";
     public static final String FIND_CAR_BY_PHONE_NUMBER_MESSAGE = "Найти машину по номеру телефона.";
-    public static final String CAR_FOUND_SUCCESS_MESSAGE = "Машина успешно найдена. Может вернуть пустой список если машины не найдены.";
+    public static final String CAR_FOUND_SUCCESS_MESSAGE =
+            "Машина успешно найдена. Может вернуть пустой список если машины не найдены.";
+    public static final String ACCESS_ADMIN_USER_MESSAGE =
+            "Доступ: ADMIN - все машины, USER - только машины, которые фигурируют в пропусках пользователя";
+    public static final String ACCESS_ADMIN_MANAGER_SECURITY_USER_MESSAGE =
+            "Доступ: ADMIN - любые машины, MANAGER, SECURITY - только машины, относящиеся к их территории, " +
+                    "USER - только машины, которые фигурируют в пропусках данного пользователя";
 
+    //для CrossingEventController
+    public static final String EVENTS_ARE_FOUND_MESSAGE = "События найдены";
 
-
-
-
-
-
-
-
+    //для PassController
+    public static final String PASSES_ARE_FOUND_MESSAGE = "Пропуска найдены";
+    public static final String PASS_NOT_FOUND_MESSAGE = "Пропуск не найден";
+    public static final String PASS_ACCESS_ALL_MESSAGE =
+            "Доступ: ADMIN - пропуски всех пользователей, MANAGER, SECURITY, USER - только свои";
 }
