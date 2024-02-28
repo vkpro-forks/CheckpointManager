bf6ceec23e4c7cb52dcb2c7a29673c9ff3325a10
Rnd-mi <11.stepanov.01@gmail.com>
2024-02-28 16:02:23 +0300

    feat(314): user-territory relation definition method with userId parameter in TerritoryAuthFacade

--- src/main/java/ru/ac/checkpointmanager/security/authfacade/TerritoryAuthFacade.java
+++ src/main/java/ru/ac/checkpointmanager/security/authfacade/TerritoryAuthFacade.java
@@ -2,6 +2,7 @@ package ru.ac.checkpointmanager.security.authfacade;
 
 import org.springframework.stereotype.Component;
 import ru.ac.checkpointmanager.repository.TerritoryRepository;
+import ru.ac.checkpointmanager.model.enums.Role;
 
 import java.util.UUID;
 
@@ -18,5 +19,18 @@ public final class TerritoryAuthFacade implements AuthFacade {
     public boolean isIdMatch(UUID territoryId) {
         UUID userId = getCurrentUser().getId();
         return territoryRepository.checkUserTerritoryRelation(userId, territoryId);
+    }
+
+    /**
+     * Проверка, относится ли пользователь ({@link Role#USER})
+     * с переданным Id и текущий пользователь ({@link Role#MANAGER}, {@link Role#SECURITY}) к одной территории
+     *
+     * @param userId UUID пользователя, которого необходимо проверить на совпадение.
+     * @return true - если пользователи относятся к одной территории,
+     * false - если нет
+     */
+    public boolean isUserIdMatch(UUID userId) {
+        UUID currentUserId = getCurrentUser().getId();
+        return territoryRepository.checkIfUsersAreInTheSameTerritory(currentUserId, userId);
     }
 }
