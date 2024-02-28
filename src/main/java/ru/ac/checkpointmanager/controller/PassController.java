8e90ce7a62fa6f8f38e2b905f3741f00d5a91cd4
Rnd-mi <11.stepanov.01@gmail.com>
2024-02-28 19:36:45 +0300

    chore(314): delete unused import

--- src/main/java/ru/ac/checkpointmanager/controller/PassController.java
+++ src/main/java/ru/ac/checkpointmanager/controller/PassController.java
@@ -29,7 +29,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ru.ac.checkpointmanager.annotation.AllRolesPreAuthorize;
 import ru.ac.checkpointmanager.annotation.IsAdminOrUserWhoOwnsPass;
 import ru.ac.checkpointmanager.annotation.PagingParam;
 import ru.ac.checkpointmanager.dto.passes.FilterParams;
