e06f5d92fd1c58a4d15c3df848ad5a2071d74b1b
x3imal <aasurov@gmail.com>
2024-03-01 12:40:47 +0300

    338: продолжаю изменять настройки конфигов

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-#логин на docker hub
+# Логин на docker hub
 docker login -u aasurov -p "$DOCKER_HUB_TOKEN"
 
+# Шаг 1: Получение ID контейнера с фронтом
+FRONTEND_CONTAINER_ID=$(docker ps -qf "name=root-frontend-1")
 
-# Шаг 1: Удаление ВСЕХ контейнеров и ВСЕХ образов независимо работают они или нет
-docker stop $(docker ps -q) && docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
+# Шаг 2: Удаление всех контейнеров, кроме контейнера с фронтом
+docker stop $(docker ps -q | grep -v $FRONTEND_CONTAINER_ID) && docker rm $(docker ps -aq | grep -v $FRONTEND_CONTAINER_ID)
 
-# Шаг 2: Загрузка образов с Docker Hub
+# Удаление всех образов, кроме образа с фронтом
+docker images -q | grep -v $(docker images -q aasurov/anvilcoder-frontend) | xargs -r docker rmi
+
+# Шаг 3: Загрузка образов с Docker Hub
 docker pull aasurov/anvilcoder:checkpoint-manager-$PROJECT_VERSION
 
-# Шаг 3: Запуск контейнеров с новыми образами
+# Шаг 4: Запуск контейнеров с новыми образами
 docker-compose -f docker-compose.prod.yml up -d
 
-# Шаг 4: Удаление всех файлов внутри каталога
+# Шаг 5: Удаление всех файлов внутри каталога
 rm -f app.jar deploy.sh docker-compose.prod.yml Dockerfile .env
--- src/main/java/ru/ac/checkpointmanager/configuration/OpenApiConfig.java
+++ src/main/java/ru/ac/checkpointmanager/configuration/OpenApiConfig.java
@@ -32,7 +32,7 @@ public class OpenApiConfig {
     @Profile("prod")
     public OpenAPI openAPIProd() {
         return new OpenAPI()
-                .addServersItem(new Server().url("https://checkpoint-manager.ru/swagger"))
+                .addServersItem(new Server().url("https://checkpoint-manager.ru"))
                 .info(new Info().title("Checkpoint Manager")
                         .description(SWAGGER_DESCRIPTION_MESSAGE)
                         .version(appVersion));
