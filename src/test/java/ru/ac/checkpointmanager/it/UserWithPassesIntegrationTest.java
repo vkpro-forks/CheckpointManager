2a1027b6350970a6dfc7be8f98d517db13bdf099
Veselov Nikolay <veselovnd@gmail.com>
2024-03-05 12:53:58 +0300

    chore(342): tested, changelogs in yaml moved to separate directory

--- src/test/java/ru/ac/checkpointmanager/it/UserWithPassesIntegrationTest.java
+++ src/test/java/ru/ac/checkpointmanager/it/UserWithPassesIntegrationTest.java
@@ -1,6 +1,7 @@
 package ru.ac.checkpointmanager.it;
 
 import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.ac.checkpointmanager.config.EnablePostgresAndRedisTestContainers;
 import ru.ac.checkpointmanager.extension.SavedPassWithRequiredEntitiesDTO;
 import ru.ac.checkpointmanager.extension.annotation.PassWithRequiredEntities;
+import ru.ac.checkpointmanager.repository.UserRepository;
 import ru.ac.checkpointmanager.util.MockMvcUtils;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,11 +29,16 @@ class UserWithPassesIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    UserRepository userRepository;
+
     //https://junit.org/junit5/docs/current/user-guide/#extensions extension model in action
     @Test
     @SneakyThrows
     void deleteUser_UserWithPass_DeleteAndReturnNoContent(@PassWithRequiredEntities SavedPassWithRequiredEntitiesDTO passWithRequiredEntitiesDTO) {
         mockMvc.perform(MockMvcUtils.deleteUser(passWithRequiredEntitiesDTO.getUser().getId()))
                 .andExpect(status().isNoContent());
+
+        Assertions.assertThat(userRepository.existsByEmail(passWithRequiredEntitiesDTO.getUser().getEmail())).isFalse();
     }
 }
