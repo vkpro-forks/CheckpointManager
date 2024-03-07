d29fa1aa49b40c06c5449410ff9f5ae48bb17ccb
Veselov Nikolay <veselovnd@gmail.com>
2024-03-07 09:57:10 +0300

    test(342): added checking of pass in DB with null user field

--- src/test/java/ru/ac/checkpointmanager/it/UserWithPassesIntegrationTest.java
+++ src/test/java/ru/ac/checkpointmanager/it/UserWithPassesIntegrationTest.java
@@ -13,6 +13,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.ac.checkpointmanager.config.EnablePostgresAndRedisTestContainers;
 import ru.ac.checkpointmanager.extension.SavedPassWithRequiredEntitiesDTO;
 import ru.ac.checkpointmanager.extension.annotation.PassWithRequiredEntities;
+import ru.ac.checkpointmanager.model.passes.Pass;
+import ru.ac.checkpointmanager.repository.PassRepository;
 import ru.ac.checkpointmanager.repository.UserRepository;
 import ru.ac.checkpointmanager.util.MockMvcUtils;
 
@@ -32,13 +34,19 @@ class UserWithPassesIntegrationTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PassRepository passRepository;
+
     //https://junit.org/junit5/docs/current/user-guide/#extensions extension model in action
     @Test
     @SneakyThrows
-    void deleteUser_UserWithPass_DeleteAndReturnNoContent(@PassWithRequiredEntities SavedPassWithRequiredEntitiesDTO passWithRequiredEntitiesDTO) {
+    void deleteUser_UserWithPass_DeleteAndReturnNoContent(@PassWithRequiredEntities
+                                                          SavedPassWithRequiredEntitiesDTO passWithRequiredEntitiesDTO) {
         mockMvc.perform(MockMvcUtils.deleteUser(passWithRequiredEntitiesDTO.getUser().getId()))
                 .andExpect(status().isNoContent());
 
         Assertions.assertThat(userRepository.existsByEmail(passWithRequiredEntitiesDTO.getUser().getEmail())).isFalse();
+        Assertions.assertThat(passRepository.findById(passWithRequiredEntitiesDTO.getPass().getId())).isPresent().get()
+                .extracting(Pass::getUser).isNull();
     }
 }
