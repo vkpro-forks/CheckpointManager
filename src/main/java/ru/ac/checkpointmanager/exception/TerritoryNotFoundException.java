25dac2e33b094e1b2b384044514f271ac4ab388d
Ldv236 <Ldv236@yandex.ru>
2024-03-14 21:41:22 +0200

    354: upgrade exceptions

--- src/main/java/ru/ac/checkpointmanager/exception/TerritoryNotFoundException.java
+++ src/main/java/ru/ac/checkpointmanager/exception/TerritoryNotFoundException.java
@@ -2,10 +2,15 @@ package ru.ac.checkpointmanager.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 
+import java.util.UUID;
+
 public class TerritoryNotFoundException extends EntityNotFoundException {
 
     public TerritoryNotFoundException(String message) {
         super(message);
     }
 
+    public TerritoryNotFoundException(UUID territoryId) {
+        super(String.format("Territory with id [%s] not found", territoryId));
+    }
 }
