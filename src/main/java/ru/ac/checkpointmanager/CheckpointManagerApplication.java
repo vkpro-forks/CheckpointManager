dee34092ed3426740a46c49d31a288a4b03dd22e
Veselov Nikolay <veselovnd@gmail.com>
2024-03-16 21:27:13 +0300

    feat(78): springdoc preload, caching, moved scheduling config to separate class

--- src/main/java/ru/ac/checkpointmanager/CheckpointManagerApplication.java
+++ src/main/java/ru/ac/checkpointmanager/CheckpointManagerApplication.java
@@ -4,13 +4,11 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 
 @SpringBootApplication
-@EnableScheduling
 @EnableEncryptableProperties
 @EnableCaching
 public class CheckpointManagerApplication {
