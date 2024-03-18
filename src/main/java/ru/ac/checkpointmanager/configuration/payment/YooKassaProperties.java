ffb072f319bcfbe353d1007f6d439f0486de5028
Veselov Nikolay <veselovnd@gmail.com>
2024-03-18 17:50:08 +0300

    feat(135): YooKassaProperties

---
+++ src/main/java/ru/ac/checkpointmanager/configuration/payment/YooKassaProperties.java
@@ -0,0 +1,20 @@
+package ru.ac.checkpointmanager.configuration.payment;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "yookassa")
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class YooKassaProperties {
+
+    Integer shopId;
+
+    String secretKey;
+
+    String url;
+}
