ba863d39cda5f30600789faf7b5559971cd7f04a
Ldv236 <Ldv236@yandex.ru>
2024-03-23 16:38:14 +0200

    156: condition for aspect

--- src/main/java/ru/ac/checkpointmanager/aspects/ServiceCallLoggingAspect.java
+++ src/main/java/ru/ac/checkpointmanager/aspects/ServiceCallLoggingAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 @Component
 @Aspect
 @Slf4j
+@ConditionalOnProperty(name = "logging.aspect.call-service", havingValue = "true")
 public class ServiceCallLoggingAspect {
 
     /**
