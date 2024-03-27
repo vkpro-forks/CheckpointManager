feecc0126db97810f0ea5f126e99020be8f9a5f6
Cardinansk <andron.nsk.54@gmail.com>
2024-03-27 22:26:22 +0700

    feat(167): Вынес название БД в переменную окружения, поменял путь контекста для Dockerfile-setup-postgres, поправил пути

--- config/postgres/init-db.sh
+++ config/postgres/init-db.sh
@@ -3,7 +3,7 @@ set -e
 
 # Добавляем плагин в библиотеку и указываем с какой БД работать
 echo "shared_preload_libraries = 'pg_cron'" >> /var/lib/postgresql/data/postgresql.conf
-echo "cron.database_name = 'chpmanDB'" >> /var/lib/postgresql/data/postgresql.conf
+echo "cron.database_name = '${CRON_DB_NAME}'" >> /var/lib/postgresql/data/postgresql.conf
 
 # Перезапускаем Postgres
 /usr/lib/postgresql/16/bin/pg_ctl restart
@@ -18,7 +18,6 @@ EOSQL
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     ${CRON_SCHEDULE_CLEAR}
 EOSQL
-
 
 # Добавление задания в pg_cron в виде: (NAME, CRON, SQL_QUERY)
 # Пример: SELECT cron.schedule('Delete logs','55 23 * * *', 'DELETE FROM logs WHERE time < now() - interval ''1 day''');
